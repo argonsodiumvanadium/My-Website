6b7e51c5271e6d69bf7a2e7494fba908adf08ab8
arg0n_50d1um-V4naD1um <51513216+argonsodiumvanadium@users.noreply.github.com>
2020-08-05 11:31:49 +0530

    usage of API branch is in effect

--- src/index.js
+++ src/index.js
@@ -1,20 +1,64 @@
-var PROJECTS = [
-	{
-		"name"   : "Animegami",
-		"done"   : "5",
-		"status" : "paused",
-		"class"  : "warning",
-		"link"   : "projects/animegami.html"
-	}
-]
+var $projects = null
+var $blogs = null
+var $technologies = null
+var $myStatus = null
 
 gen = arg => document.createElement(arg)
 $   = arg => document.getElementById(arg)
 
-loadProjects = () => {
+getProjectStatus = async () => {
+	await fetch ("https://raw.githubusercontent.com/argonsodiumvanadium/my-website/api/status.json").
+	then  (response => response.text()).
+	then  (result   => {console.log(result);$projects = JSON.parse(result);console.log($projects)}).
+	catch (err => console.log(err))
+}
+
+getBlogs = async () => {
+	await fetch ("https://raw.githubusercontent.com/argonsodiumvanadium/my-website/api/blog.json").
+	then  (response => response.text()).
+	then  (result   => {$blogs = JSON.parse(result)}).
+	catch (err => console.log(err))
+}
+
+getTechnoList = async () => {
+	await fetch ("https://raw.githubusercontent.com/argonsodiumvanadium/my-website/api/technologies.html").
+	then  (response => response.text()).
+	then  (result   => {$technologies = result}).
+	catch (err => console.log(err))
+}
+
+getMyStatus = async () => {
+	await fetch("https://raw.githubusercontent.com/argonsodiumvanadium/my-website/api/index-status.html").
+	then  (response => response.text()).
+	then  (result   => {$myStatus = result}).
+	catch (err => console.log(err))
+}
+
+loadBlogs = async () => {
+	await getBlogs()
+
+	if ($blogs.src != null) {
+		for (blog of $blogs.src) {
+			div = gen("div")
+			div.innerHTML += `
+				<div class="pt-6 mt-6 columns is-mobile">
+					<div class="column"></div>
+					<div class="notification is-dark column is-10 px-4 py-6" style="border-style: solid;border-color: #ffdd57">
+						${blog.data}
+					</div>
+					<div class="column"></div>
+				</div>
+			`
+			$("blog").prependChild(div)
+		}
+	}
+}
+
+loadProjects = async () => {
+	await getProjectStatus()
 	div = gen("div")
 
-	for (project of PROJECTS) {
+	for (project of $projects["src"]) {
 		div.innerHTML += `
 			<div onclick="window.open('${project.link}','_self')" class="mt-6 mb-2" style="cursor:pointer">
 				<h2 class="has-text-${project.class}">Animegami</h2>
@@ -26,6 +70,23 @@ loadProjects = () => {
 	}
 
 	$("projects").appendChild(div)
+}
+
+
+loadTechnoList = async() => {
+	await getTechnoList()
+	div = gen("div")
+	div.innerHTML = $technologies
+
+	$("techno-list").appendChild(div)
+}
+
+loadMyStatus = async() => {
+	await getMyStatus()
+	div = gen("div")
+	div.innerHTML = $myStatus
+
+	$("my-status").appendChild(div)
 }
 
 copyToClipboard = (data,message) => {
